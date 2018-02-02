838073640e977af70f5d2c5428fceba5f232ac57
Cyrus <forty-7@yandex.ru>
2018-02-02 18:28:02 +0300

    added boardwell role dependency

--- src/modules/Dashboard/components/Home.js
+++ src/modules/Dashboard/components/Home.js
@@ -1,25 +1,50 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-import {
-    Row,
-    Col,
-    Well,
-} from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 
-const Home = () => {
+import BoardWell from './BoardWell'
+
+const Home = (props) => {
+    const { roles } = props
+    const wells = []
+
+    if (roles.indexOf('ROLE_ADMIN') > -1) {
+        wells.push(
+            <BoardWell
+                key={0}
+                to="/"
+                title="Only admin can see this block" />
+        )
+    }
+
+    wells.push(
+        <BoardWell
+            key={1}
+            to="/dashboard/player"
+            title="Player" />
+    )
+
     return (
         <Row>
-            <Col
-                lg={4}
-                md={4}
-                sm={6}
-                xs={12}>
-                <Well>
-                    Player
-                </Well>
-            </Col>
+            {wells}
         </Row>
     )
 }
 
-export default Home
+Home.defaultProps = {
+    roles: [],
+}
+
+Home.propTypes = {
+    roles: PropTypes.array,
+}
+
+const mapStateToProps = state => {
+    return {
+        roles: state.auth.roles,
+    }
+}
+
+export default connect(mapStateToProps)(Home)
