552cf15700056720ecccfbc606a657ea85ddaf1d
Cyrus <forty-7@yandex.ru>
2018-02-02 18:29:18 +0300

    added new methods

--- src/reducers/playerReducer.js
+++ src/reducers/playerReducer.js
@@ -1,12 +1,17 @@
+import { NotificationManager } from 'react-notifications'
+
 import {
+    FETCH_DELETE_TRACK_FAILED,
+    FETCH_DELETE_TRACK_SUCCESS,
     FETCH_TRACKS_FAILED,
     FETCH_TRACKS_REQUEST,
-    FETCH_TRACKS_SUCCESS,
+    FETCH_TRACKS_SUCCESS, FETCH_UPDATE_TRACK_FAILED,
     FETCH_UPDATE_TRACK_SUCCESS,
 } from '../modules/Dashboard/const'
 
 const initialState = {
     fetching: false,
+    error: null,
     tracks: [],
 }
 
@@ -17,6 +22,7 @@ const tracksReducer = (state = initialState, action) => {
         return {
             ...state,
             fetching: true,
+            error: null,
         }
     }
 
@@ -24,22 +30,64 @@ const tracksReducer = (state = initialState, action) => {
         return {
             ...state,
             fetching: false,
+            error: null,
             tracks: action.payload,
         }
     }
 
     if (action.type === FETCH_TRACKS_FAILED) {
+        const error = action.payload
+
+        NotificationManager.error(`${error.code} ${error.message}: can not load track list`)
+
         return {
             ...state,
             fetching: false,
+            error,
         }
     }
 
     // Update track
 
     if (action.type === FETCH_UPDATE_TRACK_SUCCESS) {
+        NotificationManager.success('Track is saved')
+
         return {
             ...state,
+            error: null,
+        }
+    }
+
+    if (action.type === FETCH_UPDATE_TRACK_FAILED) {
+        const error = action.payload
+
+        NotificationManager.error(`${error.code} ${error.message}: can not update track`)
+
+        return {
+            ...state,
+            error: action.payload,
+        }
+    }
+
+    // Delete track
+
+    if (action.type === FETCH_DELETE_TRACK_SUCCESS) {
+        NotificationManager.success('Track is deleted')
+
+        return {
+            ...state,
+            error: null,
+        }
+    }
+
+    if (action.type === FETCH_DELETE_TRACK_FAILED) {
+        const error = action.payload
+
+        NotificationManager.error(`${error.code} ${error.message}: can not delete track`)
+
+        return {
+            ...state,
+            error,
         }
     }
 
