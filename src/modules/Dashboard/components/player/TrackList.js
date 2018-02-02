8e731bed54f3592d6781bae0728b1688378f8ac0
Cyrus <forty-7@yandex.ru>
2018-02-02 18:31:50 +0300

    added new methods

--- src/modules/Dashboard/components/player/TrackList.js
+++ src/modules/Dashboard/components/player/TrackList.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { NotificationManager } from 'react-notifications'
 
+import { Button } from 'react-bootstrap'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 
@@ -8,14 +11,23 @@ import FontAwesome from 'react-fontawesome'
 
 import TypeEditor from './TypeEditor'
 
-import { fetchTracksRequest, updateTrackRequest } from '../../action'
+import {
+    fetchTracksRequest,
+    updateTrackRequest,
+    deleteTrackRequest,
+} from '../../action'
 
-const beforeSaveCell = (row, cellName, cellValue) => {
+const afterDeleteRow = ({ dispatch }) => rowKyes => {
+    dispatch(deleteTrackRequest(rowKyes[0]))
+}
+
+const beforeSaveCell = () => (row, cellName, cellValue) => {
+
     if (cellName === 'bpm') {
         cellValue = +cellValue
 
         if (isNaN(cellValue)) {
-            alert('Value must be integer!')
+            NotificationManager.error('bpm value must be integer!')
 
             return false
         }
@@ -24,20 +36,18 @@ const beforeSaveCell = (row, cellName, cellValue) => {
     return true
 }
 
-const afterSaveCell = dispatch => {
-    return row => {
-        dispatch(
-            updateTrackRequest({
-                id: row.id,
-                name: row.name,
-                src: row.src,
-                bpm: row.bpm,
-                classic: row.type.classic ? 1 : 0,
-                jnj: row.type.jnj ? 1 : 0,
-                beg: row.type.beg ? 1 : 0,
-            })
-        )
-    }
+const afterSaveCell = ({ dispatch }) => row => {
+    dispatch(
+        updateTrackRequest({
+            id: row.id,
+            name: row.name,
+            src: row.src,
+            bpm: +row.bpm,
+            classic: row.type.classic ? 1 : 0,
+            jnj: row.type.jnj ? 1 : 0,
+            beg: row.type.beg ? 1 : 0,
+        })
+    )
 }
 
 const typeFormatter = (cell) => {
@@ -73,6 +83,14 @@ const createTypeEditor = (onUpdate, props) => (
 // eslint-disable-next-line react/no-multi-comp
 class TrackList extends React.Component {
     componentWillMount() {
+        this.refreshTable()
+    }
+
+    handleClickRefreshButton = () => {
+        this.refreshTable()
+    }
+
+    refreshTable = () => {
         const { dispatch } = this.props
 
         dispatch(fetchTracksRequest())
@@ -82,7 +100,6 @@ class TrackList extends React.Component {
         const {
             fetching,
             tracks,
-            dispatch,
         } = this.props
 
         if (fetching) {
@@ -96,11 +113,40 @@ class TrackList extends React.Component {
             )
         }
 
+        const tableOptions = {
+            afterDeleteRow: afterDeleteRow(this.props),
+            btnGroup: props => {
+                const buttons = []
+
+                Object.keys(props).forEach((key, i) => {
+                    if (props[key]) {
+                        const e = React.cloneElement(props[key], { key: i })
+
+                        buttons.push(e)
+                    }
+                })
+
+                buttons.push(
+                    <Button
+                        type="button"
+                        onClick={this.handleClickRefreshButton}>
+                        <FontAwesome name="refresh" /> Refresh
+                    </Button>
+                )
+
+                return buttons
+            },
+        }
+
+        const selectRow = {
+            mode: 'radio',
+        }
+
         const cellEdit = {
             mode: 'dbclick',
             blurToSave: true,
-            beforeSaveCell,
-            afterSaveCell: afterSaveCell(dispatch),
+            beforeSaveCell: beforeSaveCell(this.props),
+            afterSaveCell: afterSaveCell(this.props),
         }
 
         const data = tracks.map(track => {
@@ -113,23 +159,28 @@ class TrackList extends React.Component {
                     classic: track.classic,
                     jnj: track.jnj,
                     beg: track.beg,
-                }
+                },
             }
         })
 
         return (
             <BootstrapTable
                 data={data}
+                options={tableOptions}
                 pagination={true}
                 exportCSV={true}
                 csvFileName="rh-player-tracks.csv"
                 striped={true}
                 search={true}
-                cellEdit={cellEdit}>
+                selectRow={selectRow}
+                cellEdit={cellEdit}
+                insertRow={true}
+                deleteRow={true}>
                 <TableHeaderColumn
                     isKey={true}
                     dataField="id"
-                    dataSort={true}>
+                    dataSort={true}
+                    width="100px">
                     ID
                 </TableHeaderColumn>
                 <TableHeaderColumn dataField="name">
@@ -140,13 +191,15 @@ class TrackList extends React.Component {
                 </TableHeaderColumn>
                 <TableHeaderColumn
                     dataField="bpm"
-                    dataSort={true}>
+                    dataSort={true}
+                    width="75px">
                     bpm
                 </TableHeaderColumn>
                 <TableHeaderColumn
                     dataField="type"
                     dataFormat={typeFormatter}
-                    customEditor={{ getElement: createTypeEditor }}>
+                    customEditor={{ getElement: createTypeEditor }}
+                    width="150px">
                     type
                 </TableHeaderColumn>
             </BootstrapTable>
@@ -154,7 +207,13 @@ class TrackList extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
+TrackList.propTypes = {
+    dispatch: PropTypes.func,
+    fetching: PropTypes.bool,
+    tracks: PropTypes.array,
+}
+
+const mapStateToProps = state => {
     return {
         fetching: state.player.fetching,
         tracks: state.player.tracks,
